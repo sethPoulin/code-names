e65e6b13bede91dc021e12844e1f680813dccc95
sethPoulin <seth.poulin@gmail.com>
2021-01-03 16:22:12 -0500

    setup read and write from firebase, remove vuefire

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
+const RecentDocuments = () => import('../views/RecentDocuments.vue')
+
 const routes = [
   {
     path: '/',
@@ -17,6 +19,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/recent',
+    name: 'recent',
+    component: RecentDocuments
   }
 ]
 
