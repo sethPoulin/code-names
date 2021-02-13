6a397ad781a3e04f2783611f3878fe34f4b6d7e3
sethPoulin <seth.poulin@gmail.com>
2021-02-13 15:24:00 -0500

    add start of error message on create page

--- src/views/CreateGame.vue
+++ src/views/CreateGame.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
-    <form action="">
+    <h1>CODE NAMES</h1>
+    <form v-on:submit.prevent="setupGame()">
       <label for="game">Enter a name for your game: </label>
-      <input v-model="gameName" type="text" id="game" placeholder="Ex: my-super-fun-game">
+      <p v-if="!inputIsValid" class="error-message">Please enter a name with at least 3 characters</p>
+      <input v-model="gameName" type="text" id="game" placeholder="Ex: kowabunga">
     </form>
-    <button @click="setupGame()">Create new game</button>
+    <base-button @click.native="setupGame()">Create new game</base-button>
     <p>{{ data.username }}</p>
   </div>
 </template>
@@ -13,16 +15,21 @@
 <script>
 import { db } from '@/db.js'
 import { wordList } from '@/utils/wordlist.service.js'
+import BaseButton from '@/components/BaseButton.vue'
 // import { teamData } from '@/utils/teamData.service.js'
 
 export default {
   name: 'recent',
+  components: {
+    BaseButton
+  },
   data() {
     return {
       gameName: '',
       data: '',
       gameId: undefined,
-      cardList: undefined
+      cardList: undefined,
+      inputIsValid: false
       // redCards: undefined,
       // blueCards: undefined
     }
@@ -83,4 +90,53 @@ export default {
 }
 // reference: https://firebase.google.com/docs/database/web/read-and-write
 </script>
+
+<style lang="stylus" scoped>
+
+  * 
+   margin-top: 0
+
+  h1 
+    margin-top: 8rem
+    font-size: 3rem
+    letter-spacing: 0.3rem
+
+
+  h1,
+  label
+    color: rgb(198, 198, 198)
+
+  label 
+    font-weight: bold
+    letter-spacing: 0.1rem
+  
+  label,
+  input 
+    display: block
+    margin: 0 auto
+    margin-bottom: 2rem
+    font-size: 1.6rem
+
+  input 
+    padding: 0.8rem 1.2rem 
+    text-align: center 
+    color: rgb(64, 64, 64)
+    border-radius: 8px
+
+  button 
+    text-transform: uppercase
+    font-weight: bold
+    font-size: 1.1rem
+
+  .error-message 
+    color: rgba(210, 67, 51, 1)
+    font-size: 1.4rem
+    font-weight: bold
+    background: white
+    display: inline-block 
+    text-align: center
+    border-radius: 8px
+    padding: 0.4rem 1.2rem
+
+</style>
 
